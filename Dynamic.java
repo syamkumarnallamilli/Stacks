c69a79d4ee6c5d83917dcf7f18fadcc805c8112e
Chanakya-dev <Chanakyamanas123@gmail.com>
2024-07-24 12:55:28 +0530

    Create Dynamic.java

---
+++ Dynamic.java
@@ -0,0 +1,8 @@
+public class Dynamic{
+public static void main(String[]args){
+// Create an Object for Stack
+  // Load person class Objects in it using push function
+  // Iterate it by using List Iterator
+  // print the values by using while Loop
+}
+}
