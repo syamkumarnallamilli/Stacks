c447ef9920a86b22b58b0f221571647890b15a96
Chanakya-dev <Chanakyamanas123@gmail.com>
2024-07-24 12:56:47 +0530

    Create Enumerations.java

---
+++ Enumerations.java
@@ -0,0 +1,8 @@
+public class Enumerations{
+  public static void main(String[]args){
+   // Create an Object for Stack
+  // Load person class Objects in it
+  // Iterate it by using Enumeration
+  // print the values by using while Loop
+
+  }}
